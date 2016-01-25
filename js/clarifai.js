9ab96f62d182a94d2515ac710875d39e249a9e7e
pnanthakijjar <poonyapornnanthakijjar@wc-dhcp141d203.student-secure.wireless.fsu.edu>
2016-01-25 10:19:03 -0500

    Version 1.0

---
+++ js/clarifai.js
@@ -0,0 +1,64 @@
+
+var tags2;
+
+function getCredentials() {
+var data = {
+      'grant_type': 'client_credentials',
+      'client_id': "fUhst7tpAZGWhPKyJpX2yTYLQmuOXBk_O77bXeiD",
+      'client_secret': "IQw-agHWk3WwnKf4Q_hke8FjcIRzkdxFO2kv7VmC"
+    };
+
+    $.ajax({
+      'url': 'https://api.clarifai.com/v1/token',
+      'data': data,
+      'type': 'POST'
+    })
+    .then(function(r) {
+      localStorage.setItem('accessToken', r.access_token);
+    });
+}
+
+function postImage(imgurl) {
+    var data = {
+      'url': imgurl
+    };
+    var accessToken = localStorage.getItem('accessToken');
+
+    return $.ajax({
+      'url': 'https://api.clarifai.com/v1/tag',
+      'headers': {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      'data': data,
+      'type': 'POST'
+    }).then(function(r){
+        localStorage.setItem('tagResp', r);
+        parseResponse(r);
+
+    });
+  }
+
+  function parseResponse(resp) {
+    var tags = [];
+    if (resp.status_code === 'OK') {
+      var results = resp.results;
+      tags = results[0].result.tag.classes;
+      tags2 = tags;
+    } else {
+      console.log('Sorry, something is wrong.');
+    }
+    console.log(tags);
+    
+     var ldata = jQuery('#lData ul');
+     tags.forEach(function(tag) {
+         console.log(tag);
+        ldata.append('<li class="tag">'+tag+'</li>'); 
+     });
+
+    return tags;
+  }
+
+  function run(imgurl) {
+    $.when(getCredentials())
+    .then(postImage(imgurl))
+}
---
+++ js/keys.js
@@ -0,0 +1,2 @@
+var CLIENT_ID = "fUhst7tpAZGWhPKyJpX2yTYLQmuOXBk_O77bXeiD";
+var CLIENT_SECRET = "IQw-agHWk3WwnKf4Q_hke8FjcIRzkdxFO2kv7VmC";
